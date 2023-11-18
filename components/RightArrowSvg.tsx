f30d97634ad525756e899f8c39f75e76708e7f91
GoorezyEST <goorezyestudios@gmail.com>
2023-11-18 19:33:30 -0300

    All the necessary SVGs for the web app

---
+++ components/RightArrowSvg.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function RightArrowSvg() {
+  return (
+    <svg
+      width="800px"
+      height="800px"
+      viewBox="0 0 20 20"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="layer1">
+        <path
+          d="M 8,5.2910156 7.2929688,6 7.6464844,6.3535156 11.292969,10 7.6464844,13.646484 7.2929688,14 8,14.708984 8.3535156,14.353516 12.708984,10 8.3535156,5.6464844 Z"
+          style={{ fill: "var(--primary)" }}
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default RightArrowSvg;
