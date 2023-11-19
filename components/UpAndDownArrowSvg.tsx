710ed7e8a6c943eb7ad1e695e4ade5bb0f2f17fe
GoorezyEST <goorezyestudios@gmail.com>
2023-11-19 16:43:51 -0300

    New SVGS and fixed decorativeSvg

---
+++ components/UpAndDownArrowSvg.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function UpAndDownArrowSvg() {
+  return (
+    <svg
+      width="800px"
+      height="800px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 4V20M12 4L8 8M12 4L16 8"
+        stroke="var(--background)"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default UpAndDownArrowSvg;
