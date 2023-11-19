f629a4eacdb1afb2d178af1c24f99cf5ee700010
GoorezyEST <goorezyestudios@gmail.com>
2023-11-19 16:42:59 -0300

    Added a loading text

--- context/LoadingScreen.js
+++ context/LoadingScreen.js
@@ -7,7 +7,7 @@ import LogoSvg from "@/components/LogoSvg";
 import { AnimatePresence, motion } from "framer-motion";
 
 export function LoadingScreen({ children }) {
-  const { isHydrated } = useGlobal();
+  const { isHydrated, langSettings } = useGlobal();
 
   useEffect(() => {
     if (!isHydrated) {
@@ -22,17 +22,18 @@ export function LoadingScreen({ children }) {
         {isHydrated && (
           <motion.section
             className="loading_container"
-            initial={{ x: 0 }}
-            animate={{ x: 0 }}
-            exit={{ x: "-100%" }}
+            initial={{ y: 0 }}
+            animate={{ y: 0 }}
+            exit={{ y: "-100%" }}
             transition={{
-              duration: 0.225,
+              duration: 0.2125,
               ease: "circIn",
             }}
           >
             <div className="loading_logo">
               <LogoSvg />
             </div>
+            <span>{langSettings.loading.one}</span>
           </motion.section>
         )}
       </AnimatePresence>
